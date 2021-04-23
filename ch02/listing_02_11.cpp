b6c137f6a75c7464db72b809864f603a5dc3ad8c
Mamedov A.M <mam28@yandex.ru>
2021-04-23 16:33:44 +0800

    Глава 2. Массивы

---
+++ ch02/listing_02_11.cpp
@@ -0,0 +1,18 @@
+// Рефакторинг листинга 10 с использованием цикла
+// на основе диапазона
+
+#include <cstdio>
+
+int main()
+{
+    unsigned long maximum = 0;
+    unsigned long values[] = {10, 50, 20, 40, 0};
+
+    for (unsigned long value: values) {
+        if (value > maximum) maximum = value;
+    }
+
+    printf("The maximum value is %lu.\n", maximum);
+    
+    return 0;
+}
---
+++ ch02/listing_02_12.cpp
@@ -0,0 +1,21 @@
+// Вывод букв алфавита (англ.) в нижнем и верхнем регистрах
+// с использованием ASCII
+
+#include <cstdio>
+
+int main()
+{
+    char alphabet[27];
+    for (int i = 0; i < 26; ++i) {
+        alphabet[i] = i + 97;
+    }
+    alphabet[26] = 0;
+    printf("%s\n", alphabet);
+
+    for (int i = 0; i < 26; ++i) {
+        alphabet[i] = i + 65;
+    }
+    printf("%s\n", alphabet);
+    
+    return 0;
+}
